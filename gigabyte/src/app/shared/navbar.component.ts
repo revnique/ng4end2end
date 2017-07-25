77c3738383e72a752f8ad52592cf2cb0ae673058
revnique <revnique@gmail.com>
2017-07-25 15:04:00 -0500

    impletmented windowresize

--- gigabyte/src/app/shared/navbar.component.ts
+++ gigabyte/src/app/shared/navbar.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { WindowSize} from './windowResize'
 
 @Component({
     selector: 'navi-bar',
     template: `
                 <div class="top-bar">
-                    <div class="top-bar-title">&lt;h1&gt;teq_</div>
+                    <div class="top-bar-title">&lt;h1&gt;teq_<span>&nbsp;size:{{size}}</span></div>
                     <div>
                         <ul class="menu">
                             <li class="nav-menu"><a [routerLink]="['/admin']">admin area</a></li>
@@ -14,4 +15,15 @@ import { Component } from '@angular/core';
     `,
     styleUrls: ['./navbar.component.css']
 })
-export class NavComponent {}
+export class NavComponent implements OnInit{
+    size:number;
+    ngOnInit(){
+        this.getsize();
+    }
+    getsize(){
+        let d = new WindowSize;
+        d.height$.subscribe((h)=>this.size = h);
+    }
+
+    
+}
---
+++ gigabyte/src/app/shared/windowResize.ts
@@ -0,0 +1,23 @@
+import {Observable, BehaviorSubject} from 'rxjs';
+
+export class WindowSize {
+    width$: Observable<number>;
+    height$: Observable<number>;
+
+    constructor() {
+        let windowSize$ = new BehaviorSubject(getWindowSize());
+        this.width$ = (windowSize$.pluck('width') as Observable<number>).distinctUntilChanged();
+        this.height$ = (windowSize$.pluck('height') as Observable<number>).distinctUntilChanged();
+
+        Observable.fromEvent(window, 'resize')
+            .map(getWindowSize)
+            .subscribe(windowSize$);
+    }
+}
+
+function getWindowSize() {
+    return {
+        height: window.innerHeight,
+        width: window.innerWidth
+    };
+}
